f101e364f60b6be02caa606a420a1c5432c58118
aniel Johannsen <daniel-johannsen@web.de>
2022-02-26 17:45:08 +0100

    included models

--- js/game.js
+++ js/game.js
@@ -1,15 +1,8 @@
 let canvas;
-let ctx;
-let character = new Character();
-let enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-];
-let world = new World();
+let world;
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,14 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 180;
     img;
+    height = 250;
+    width = 120;
+
+    loadImage(path) {
+        this.img = new Image(); // this.img = document.getElementById(..) <img id=".." src="..">
+        this.img.src = path;
+    }
 
     moneRight() {
         console.log('move right');
