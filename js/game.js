68ba198c80eff256af73039ce6107016333e5bfc
aniel Johannsen <daniel-johannsen@web.de>
2022-03-19 18:53:05 +0100

    functions for mobile

--- js/game.js
+++ js/game.js
@@ -13,6 +13,14 @@ function start() {
     document.getElementById('startScreen').classList.add('d-none');
     document.getElementById('btn').classList.add('d-none');
     document.getElementById('canvas').classList.remove('d-none');
+    document.getElementById('left').classList.remove('d-none');
+    document.getElementById('right').classList.remove('d-none');
+    document.getElementById('jump').classList.remove('d-none');
+    document.getElementById('throw').classList.remove('d-none');
+}
+
+function info() {
+    document.getElementById('infoBox').classList.toggle('d-none');
 }
 
 window.addEventListener("keydown", (e) => {
@@ -56,3 +64,43 @@ window.addEventListener("keyup", (e) => {
         keyboard.D = false;
     }
 });
+
+
+function touchDownLeft() {
+    keyboard.LEFT = true;
+}
+
+
+function touchUpLeft() {
+    keyboard.LEFT = false;
+}
+
+//##
+function touchDownRight() {
+    keyboard.RIGHT = true;
+}
+
+
+function touchUpRight() {
+    keyboard.RIGHT = false;
+}
+
+//##
+function touchDownJump() {
+    keyboard.SPACE = true;
+}
+
+
+function touchUpJump() {
+    keyboard.SPACE = false;
+}
+
+//##
+function touchDownThrow() {
+    keyboard.D = true;
+}
+
+
+function touchUpThrow() {
+    keyboard.D = false;
+}
